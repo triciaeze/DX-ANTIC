c334392286074b89bb670838aff38ab12c48d85d
triciaeze <triciaeze058@gmail.com>
2022-10-18 16:39:35 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,12 @@
+var harmburger=document.querySelector(".harmburger-menu")
+// console.log(harmburger)
+
+function addandremove(){
+harmburger.classList.toggle("showburger")
+}
+var mobilelinkscontainer=document.querySelector('.mobile-links')
+
+function addandremove(){
+    harmburger.classList.toggle('showburger')
+    mobilelinkscontainer.classList.toggle("showmobilelinks")
+}
